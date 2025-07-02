4573dfd3db0fa5457ee1aa5549579590b912eafc
John Kerl <kerl.john.r@gmail.com>
2025-07-02 14:19:32 -0400

    testing

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -4,24 +4,41 @@
 # XXX COMMENTS ON CONTEXT
 # ================================================================
 
+set -euo pipefail
+
+export text_default=$'\033[0m'
+export text_red=$'\033[31;01m'
+export text_green=$'\033[32;01m'
+
 # ----------------------------------------------------------------
 # Check for existings
 echo ""
 echo "johnkerl/dotfiles: checking for overwrites"
 
-ok="true"
+# ok="true"
+# for file in \
+#   ~/.bashrc \
+#   ~/.bashrc_profile
+# do
+#   if [ -f "$file" ]; then
+#     echo "File $file exists -- please rename it as backup"
+#     ok="false"
+#   fi
+# done
+# if [ "$ok" != "true" ]; then
+#   exit 1
+# fi
+
+stamp=$(date "+%Y%m%d-%H%M%S")
 for file in \
   ~/.bashrc \
   ~/.bashrc_profile
 do
   if [ -f "$file" ]; then
-    echo "File $file exists -- please rename it as backup"
-    ok="false"
+    mv $file $file.$stamp
+    echo "${text_green}johnkerl/dotfiles: mv $file $file.$stamp${text_default}"
   fi
 done
-if [ "$ok" != "true" ]; then
-  exit 1
-fi
 
 # ----------------------------------------------------------------
 echo ""
@@ -29,26 +46,32 @@ echo "johnkerl/dotfiles: installing symlinks"
 
 for item in \
   bashrc \
-  bashrc_profile \
   bashrcs-public \
   bashrc-bootstrap \
   screenrc \
   vimrc
 do
-  if [ ! -f ./$item ]; then
-    echo johnkerl/dotfiles: cannot find ./$item
+  src=$(pwd)/$item
+  dst=~/.$item
+  if [ ! -e $src ]; then
+    echo "johnkerl/dotfiles: cannot find $src to link to $dst"
+  else
+    if [ -h $dst ]; then
+      echo "${text_green}johnkerl/dotfiles: link  $dst is okay${text_default}"
+    else
+      ln -s $src $dst
+      echo ${text_green}"johnkerl/dotfiles: ln -s $src $dst${text_default}"
+    fi
   fi
-  ln -s $(pwd)/$item ~/.$item
-  echo johnkerl/dotfiles: ln -s $(pwd)/$item ~/.$item
 done
 
 # ----------------------------------------------------------------
 if [ "$#" -eq 1 ]; then
   echo "$1" > ~/.hostname-alias
-  echo "johnnkerl/dotfiles: setting $1 in ~/.hostname-alias"
+  echo "${text_green}johnkerl/dotfiles: setting $1 in ~/.hostname-alias${text_default}"
 fi
 
 # ----------------------------------------------------------------
 echo ""
-echo "johnkerl/dotfiles: done"
+echo "${text_green}johnkerl/dotfiles: done${text_default}"
 echo ""
--- install-full.sh
+++ install-full.sh
@@ -6,6 +6,10 @@
 
 set -euo pipefail
 
+export text_default=$'\033[0m'
+export text_red=$'\033[31;01m'
+export text_green=$'\033[32;01m'
+
 # ----------------------------------------------------------------
 clone_or_pull() {
   source="$1"
@@ -19,9 +23,10 @@ clone_or_pull() {
     pushd $destination
     git pull
     popd
+    echo "${text_green}johnkerl/dotfiles: updating $destination${text_default}"
   else
     echo
-    echo "johnkerl/dotfiles: cloning $source to $destination"
+    echo "${text_green}johnkerl/dotfiles: cloning $source to $destination${text_default}"
     echo
     git clone $source $destination
   fi
@@ -46,12 +51,17 @@ echo "----------------------------------------------------------------"
 # Go ahead and try these -- the OS might require yum or brew but I most often do
 # repeat installs on Ubuntu EC2 instances so let's go with apt as the most
 # hands-off install option.
-sudo apt update -q || echo "Could not sudo apt update"
+export DEBIAN_FRONTEND=noninteractive # quiet update
+sudo apt update -qq || echo "Could not sudo apt update"
 
 # Some of my ~/scripts are in Ruby.
-sudo apt install -y -q ruby python-is-python3 || echo "Could not sudo apt install -y -q ruby python-is-python3"
+sudo apt install -y -qq ruby python-is-python3 || echo "Could not sudo apt install -y -q ruby python-is-python3"
 # Some of my ~/scripts call /usr/bin/env python when they want python3.
-sudo apt install -y -q make golang-go || echo "Could not sudo apt install -y -q make golang-go"
+sudo apt install -y -qq make golang-go || echo "Could not sudo apt install -y -q make golang-go"
+
+echo
+echo "${text_green}johnkerl/dotfiles: done trying Ruby, Python, Go${text_default}"
+echo
 
 echo
 echo "----------------------------------------------------------------"
@@ -66,8 +76,12 @@ if [ "$ok" = "true" ]; then
 
   cd ~/git/johnkerl/lumin
   go build
-  cp lumin ~/bin/
+  if [ ! -h ~/bin/lumin ]; then
+    ln -s $(pwd)/lumin ~/bin/lumin
+  fi
 
 else
   echo "Could not find Go tools"
 fi
+
+echo "${text_green}johnkerl/dotfiles: done trying full install${text_default}"
